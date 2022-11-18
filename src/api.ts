5f7a89f09c78c79391843ff8a9320b64c6916e69
KmGun <gunpoll823@gmail.com>
2022-11-18 18:48:06 +0900

    dsfsdsdff

--- src/api.ts
+++ src/api.ts
@@ -2,36 +2,37 @@ import axios from "axios";
 import { QueryStatus } from "react-query";
 import { IUserDataFromBe, IuserData, TgoogleId } from "./atoms";
 
+const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy';
 
 export function fetchGetLoginCheck(){
-    return axios.get<{loggedIn:Boolean}>(`/login-check`,{withCredentials:true}).then(res => res.data)
+    return axios.get<{loggedIn:Boolean}>(`${PROXY}/login-check`,{withCredentials:true}).then(res => res.data)
 }
 
 
 export function fetchPostUpdateCurrentUserInfoOnBE(userData : IuserData){
-    return axios.post<QueryStatus>(`/update-user-info`,userData,{withCredentials:true}).then(res => res.data)
+    return axios.post<QueryStatus>(`${PROXY}/update-user-info`,userData,{withCredentials:true}).then(res => res.data)
 }
 
 
 export function fetchGetGoogleId(){
-    return axios.get<{googleId:string}>(`/get-google-id`,{withCredentials:true}).then(res => res.data)
+    return axios.get<{googleId:string}>(`${PROXY}/get-google-id`,{withCredentials:true}).then(res => res.data)
 }
 
 export function fetchGetCurrentUserData(){
-    return axios.get<IUserDataFromBe>(`/get-current-user-data`,{withCredentials:true}).then(res => res.data)
+    return axios.get<IUserDataFromBe>(`${PROXY}/get-current-user-data`,{withCredentials:true}).then(res => res.data)
 }
 export function fetchGetMatch(){
-    return axios.get<TgoogleId[]>(`/match`,{withCredentials:true}).then(res => res.data)
+    return axios.get<TgoogleId[]>(`${PROXY}/match`,{withCredentials:true}).then(res => res.data)
 
 }
 export function fetchGetSaveYoutubeApi(){
-    return axios.get<QueryStatus>(`/youtube/save-youtube-data`,{withCredentials:true}).then(res => res.data)
+    return axios.get<QueryStatus>(`${PROXY}/youtube/save-youtube-data`,{withCredentials:true}).then(res => res.data)
 }
 
 export function fetchGetallUserDatas(){
-    return axios.get<IUserDataFromBe[]>(`/get-all-user-datas`,{withCredentials:true}).then(res => res.data)
+    return axios.get<IUserDataFromBe[]>(`${PROXY}/get-all-user-datas`,{withCredentials:true}).then(res => res.data)
 };
 
 export function fetchGetLogout(){
-    return axios.get<QueryStatus>(`/logout`,{withCredentials:true}).then(res => res.status)
+    return axios.get<QueryStatus>(`${PROXY}/logout`,{withCredentials:true}).then(res => res.status)
 }
